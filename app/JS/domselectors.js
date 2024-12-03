3510745f97b7461b3de4e3b91d7e6f92884b4d32
ninac13 <144480538+ninac13@users.noreply.github.com>
2024-12-03 13:26:07 -0500

    12/3

--- app/JS/domselectors.js
+++ app/JS/domselectors.js
@@ -1,7 +1,5 @@
 const DOMSelectors = {
     container: document.querySelector(".container"),
-    studentsButton: document.querySelector(".studentsbtn"),
-    staffButton: document.querySelector(".staffbtn"),
     slytherinButton: document.querySelector(".slytherinbtn"),
     gryffindorButton: document.querySelector(".gryffindorbtn"),
     ravenclawButton: document.querySelector(".ravenclawbtn"),
